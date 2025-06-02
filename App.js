1ca00fa3a68be71187a5e80e082a4c365de8cab7
Krunal Shah <krunal@goldenearth.io>
2025-06-02 16:32:13 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,32 @@
+const heading = React.createElement("h1",{ id: "heading"}, "Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/**
+ * 
+ *  <div id="parent" class="parent">
+ *      <div id="child1" class="child1">
+ *          <h1>Child 1 Text</h1>
+ *      </div>
+ *      <div id="child2" class="child2">
+ *          <h1>Child 2 Text</h1>
+ *      </div>
+ * <div>
+ * 
+ * 
+ */
+
+ const parent = React.createElement("div", { id : "parent" }, 
+    [
+        React.createElement("div", { id: "child1"},
+            React.createElement("h1", {}, "Child 1 Text")
+        ),
+        React.createElement("div", { id: "child2"},
+            React.createElement("h1", {}, "Child 2 Text")
+        )
+    ]
+);
+
+setTimeout(() => {
+    root.render(parent);
+}, 2000);
